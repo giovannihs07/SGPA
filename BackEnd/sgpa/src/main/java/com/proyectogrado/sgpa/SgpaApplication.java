8966bfbf8f7a2bf7700bb216b61530e70e9485cc
Giovanni Hernández <ghernandezs@unicartagena.edu.co>
2026-04-21 17:23:41 -0500

    Agrego carpeta backend sgpa con spring boot

---
+++ BackEnd/sgpa/src/main/java/com/proyectogrado/sgpa/SgpaApplication.java
@@ -0,0 +1,13 @@
+package com.proyectogrado.sgpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgpaApplication.class, args);
+	}
+
+}
